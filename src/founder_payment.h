70a97d3acd113f5225f76d6b6064e05911a8453d
subinetwork <148358622+subinetwork@users.noreply.github.com>
2023-11-08 03:30:41 +0700

    1.1.2.4

--- src/founder_payment.h
+++ src/founder_payment.h
@@ -18,7 +18,7 @@
 #include <limits.h>
 using namespace std;
 
-static const string DEFAULT_FOUNDER_ADDRESS = "GdHPW3rko8DSp3rFT7aSwkTVbXAK8GMptc";
+static const string DEFAULT_FOUNDER_ADDRESS = "STDnSyLXz5ojTm9ZNq9Lm1rLwmwzzJ7uSG";
 struct FounderRewardStructure {
 	int blockHeight;
 	int rewardPercentage;
