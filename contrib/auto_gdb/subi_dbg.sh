6626fe8375526b0da1848fccd7644eab5411291f
subinetwork <148358622+subinetwork@users.noreply.github.com>
2023-11-08 04:08:29 +0700

    1.1.2.4

--- contrib/auto_gdb/subi_dbg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# use testnet settings,  if you need mainnet,  use ~/.subicore/subid.pid file instead
-subi_pid=$(<~/.subicore/testnet3/subid.pid)
-sudo gdb -batch -ex "source debug.gdb" subid ${subi_pid}
